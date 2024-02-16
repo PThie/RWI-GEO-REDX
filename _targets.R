bb4ef2a19f06b140402dc7cd352c1cc5ab86bad9
Patrick Thiel <patrickthiel@outlook.de>
2024-02-16 21:43:20 +0100

    Basics of project setup
    Set up the most basic structure of the pipeline (like libraries).

--- _targets.R
+++ _targets.R
@@ -1,6 +1,45 @@
 #----------------------------------------------
 # description
 
+# Thhis fule is the main file that orchestrates the other coding files. It
+# controls the data pipeline and defines the global settings.
+
 #----------------------------------------------
 # load libraries
 
+suppressPackageStartupMessages({
+    library(targets)
+    library(renv)
+})
+
+#----------------------------------------------
+# set working directory
+
+setwd(here::here())
+
+#----------------------------------------------
+# load configurations
+
+source(
+    file.path(
+        here::here(),
+        "src",
+        "helpers",
+        "config.R"
+    )
+)
+
+#----------------------------------------------
+# load R scripts
+
+lapply(
+    list.files(
+        config_paths()[["code_path"]],
+        pattern = "\\.R$",
+        full.names = TRUE,
+        ignore.case = TRUE
+    ),
+    source
+)
+
+#----------------------------------------------
