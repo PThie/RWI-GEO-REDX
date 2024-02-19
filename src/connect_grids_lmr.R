f1b59635c8d5073ced466fd2880270336c069c6c
Patrick Thiel <patrickthiel@outlook.de>
2024-02-19 14:26:31 +0100

    Added a file that connects grids and labor market regions

---
+++ src/connect_grids_lmr.R
@@ -0,0 +1,34 @@
+connect_grids_lmr <- function (
+    grid_cleaned = NA,
+    labor_market_regions_cleaned = NA
+) {
+    #' @title Connecting grids and labor market regions
+    #' 
+    #' @description This file uses the geo information of the grids and the
+    #' labor market regions to connect them.
+    #' 
+    #' @param grid_cleaned Clean data sets of geo information of grids
+    #' @param labor_market_regions_cleaned Clean data sets of geo information of
+    #' labor market regions (LMR)
+    #' 
+    #' @return Dataframe connecting grids and labor market regions
+    #' @author Patrick Thiel
+
+    #----------------------------------------------
+    #  join both data sets together
+
+    grids_lmr <- sf::st_join(
+        grid_cleaned,
+        labor_market_regions_cleaned,
+        left = TRUE,
+        largest = TRUE
+    )
+
+    # remove geometry
+    grids_lmr <- sf::st_drop_geometry(grids_lmr)
+
+    #----------------------------------------------
+    # return
+
+    return(grids_lmr)
+}
