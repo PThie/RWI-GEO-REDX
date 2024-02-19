afbaee4e324da4aec9d38fbb16eac75a5dc6d98c
Patrick Thiel <patrickthiel@outlook.de>
2024-02-19 13:19:02 +0100

    Added file that cleans the labor market geo information

---
+++ src/clean_labor_market_regions.R
@@ -0,0 +1,46 @@
+clean_labor_market_regions <- function() {
+    #' @title Creating a clean labor market regions data set
+    #' 
+    #' @description This function cleans up the geographical information on the
+    #' labor market regions based on RWI defintion.
+    #' 
+    #' @return qs object
+    #' @author Patrick Thiel
+    
+    #----------------------------------------------
+    # load labor market regions
+
+    lmr <- sf::st_read(
+        file.path(
+            config_paths()[["gebiete_path"]],
+            "Arbeitsmarktregion",
+            "RWI2018",
+            "shapefiles",
+            "amr2.gpkg"
+        ),
+        quiet = TRUE
+    )
+
+    #----------------------------------------------
+    # clean labor market regions
+
+    # rename geometry column
+    sf::st_geometry(lmr) <- "geometry"
+
+    # more cleaning
+    lmr <- lmr |>
+        dplyr::select(
+            amr = AMR2,
+            geometry
+        ) |>
+        dplyr::mutate(
+            # add leading zeros to amr
+            amr = stringr::str_pad(amr, 5, pad = "0")
+        ) |>
+        sf::st_transform(config_globals()[["utmcrs"]])
+
+    #----------------------------------------------
+    # return
+
+    return(lmr)
+}
