4d1d22a9e6fa7447f7b307e51334ef172df87930
Patrick Thiel <patrickthiel@outlook.de>
2024-02-20 15:40:19 +0100

    Handling of duplicates and adding variables for regression
    Handled duplicates in apartment rents which are not captured by the algorithm
    through the combination of obid and spell since the maximum spell appears
    multiple times for the exceptions listed (same approach as for apartment
    sales where this happends as well).
    
    Added addtional variables which are needed in the next step, the estimation.
    This includes different timing variables as well as IDs as the combination
    of the regional and time level. I also added average prices for these IDs.

--- src/prepare_housing.R
+++ src/prepare_housing.R
@@ -352,12 +352,45 @@ prepare_housing <- function(
                 ) |>
                 dplyr::slice(1)
         )
+    } else if (data_type == "WM") {
+        identical_rows <- c(
+            "128172990",
+            "81330431",
+            "81769142",
+            "91352056",
+            "76735983",
+            "65522877",
+            "85809897",
+            "123143148",
+            "79930989",
+            "80125487",
+            "84395483",
+            "81421542",
+            "63487465"
+        )
+
+        org_data_unique <- org_data |>
+            dplyr::filter(
+                !obid %in% identical_rows
+            )
+
+        org_data_dup <- org_data |>
+            dplyr::filter(
+                obid %in% identical_rows
+            ) |>
+            group_by(obid) |>
+            dplyr::slice(1)
+
+        org_data <- dplyr::bind_rows(
+            org_data_unique,
+            org_data_dup
+        )
     }
 
     # UNIT TEST: no duplicates in obid
-    # tar_assert_true(
-    #     sum(duplicated(org_data$obid)) == 0
-    # )
+    tar_assert_true(
+        sum(duplicated(org_data$obid)) == 0
+    )
 
     #----------------------------------------------
     # recode all negative values to NA
@@ -387,7 +420,7 @@ prepare_housing <- function(
 
     #----------------------------------------------
     # add timing variables
-    # NOTE: the later used variables qudate and modate refer to the year-quarter
+    # NOTE: the later used variables "qudate" and "modate" refer to the year-quarter
     # and year-month of the end date
 
     org_data <- org_data |>
@@ -405,6 +438,55 @@ prepare_housing <- function(
         dplyr::select(
             -c(start_date, end_date, equarter, aquarter)
         )
+
+    #----------------------------------------------
+    # add variables for regression
+
+    for (region in c("kid2019", "gid2019", "amr", "ergg_1km")) {
+        for (time in c("ejahr", "e_year_mon", "e_year_quarter")) {
+            org_data <- org_data |>
+                dplyr::mutate(
+                    # add region-time variable
+                    !!paste0("id_", region, "_", time) := paste0(
+                        .data[[region]], "_", .data[[time]]
+                    )
+                )
+        }
+    }
+
+    #----------------------------------------------
+    # add average price per sqm for the region and time
+
+    grouping_vars <- org_data |>
+        dplyr::select(
+            dplyr::starts_with("id_")
+        ) |>
+        names()
+
+    for (var in grouping_vars) {
+        # define price variable
+        if (data_type == "WK") {
+            price_var <- "flatprice_sqmeter"
+        } else if (data_type == "WM") {
+            price_var <- "rent_sqmeter"
+        } else {
+            price_var <- "houseprice_sqmeter"
+        }
+
+        # define name of overaged price
+        name_var <- paste0("avg_", price_var, "_", substring(var, 4, nchar(var)))
+
+        # average price by time and region
+        org_data <- org_data |>
+            dplyr::group_by(
+                .data[[var]]
+            ) |>
+            dplyr::mutate(
+                # add average price per sqm for the region and time
+                !!name_var := mean(.data[[price_var]], na.rm = TRUE)
+            ) |>
+            dplyr::ungroup()
+    }
 
     #----------------------------------------------
     # return
