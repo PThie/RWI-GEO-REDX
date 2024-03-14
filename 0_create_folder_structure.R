ca3cef27840fdf1bc1b6fdd70d824e50e4982c2b
Patrick Thiel <patrickthiel@outlook.de>
2024-03-14 08:32:10 +0100

    New folder structure with new wave
    Added a script that adds corresponding folders for the new wave.

---
+++ 0_create_folder_structure.R
@@ -0,0 +1,58 @@
+#--------------------------------------------------
+# Description
+
+# This script creates the folder structure for the REDX project once
+# a new version is started.
+
+#--------------------------------------------------
+# paths
+
+main_path <- "M:/_FDZ/RWI-GEO/RWI-GEO-REDX/"
+output_path <- file.path(main_path, "output")
+
+#--------------------------------------------------
+# globals
+
+types <- c("WK", "WM", "HK")
+
+# TODO: change version folder
+version_folder <- "Dez2023"
+
+#--------------------------------------------------
+# create folders
+
+for (type in types) {
+    # output folder
+    dir.create(
+        file.path(
+            output_path,
+            type,
+            "output",
+            version_folder
+        )
+    )
+
+    # data folder
+    dir.create(
+        file.path(
+            output_path,
+            type,
+            "data",
+            version_folder
+        )
+    )
+
+    # log folder
+    dir.create(
+        file.path(
+            output_path,
+            type,
+            "log",
+            version_folder
+        )
+    )
+}
+
+
+
+
