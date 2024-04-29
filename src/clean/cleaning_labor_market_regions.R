aecf697bfdfc2333999dedd0b27b2d936f17d5b5
Patrick Thiel <patrickthiel@outlook.de>
2024-04-29 12:36:42 +0200

    Moved the file reading to its own target objects

--- src/clean/cleaning_labor_market_regions.R
+++ src/clean/cleaning_labor_market_regions.R
@@ -1,34 +1,22 @@
-cleaning_labor_market_regions <- function() {
+cleaning_labor_market_regions <- function(labor_market_regions_raw = NA) {
     #' @title Creating a clean labor market regions data set
     #' 
     #' @description This function cleans up the geographical information on the
     #' labor market regions based on RWI defintion.
     #' 
+    #' @param labor_market_regions_raw Original labor market regions
+    #' 
     #' @return qs object
     #' @author Patrick Thiel
     
     #----------------------------------------------
-    # load labor market regions
-
-    lmr <- sf::st_read(
-        file.path(
-            config_paths()[["gebiete_path"]],
-            "Arbeitsmarktregion",
-            "RWI2018",
-            "shapefiles",
-            "amr2.gpkg"
-        ),
-        quiet = TRUE
-    )
-
-    #----------------------------------------------
     # clean labor market regions
 
     # rename geometry column
-    sf::st_geometry(lmr) <- "geometry"
+    sf::st_geometry(labor_market_regions_raw) <- "geometry"
 
     # more cleaning
-    lmr <- lmr |>
+    lmr <- labor_market_regions_raw |>
         dplyr::select(
             amr = AMR2,
             geometry
