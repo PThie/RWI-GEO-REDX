ecd65df5575ce811f64bcfc12b3d40411cd31545
Patrick Thiel <patrickthiel@outlook.de>
2024-04-29 12:40:10 +0200

    Update to parquet
    Updated the reading function to parquet as the original data is now available in that format.

--- src/read/reading_housing_data_org.R
+++ src/read/reading_housing_data_org.R
@@ -1,6 +1,22 @@
-reading_housing_data_org <- function(housing_data_org_file_name = NA) {
+reading_housing_data_org <- function(data_file_path = NA) {
+    #' @title Reading the original housing data
+    #' 
+    #' @description This function reads the original housing data.
+    #' 
+    #' @param data_file_path Path to original housing data.
+    #' 
+    #' @return Data frame with the original housing data.
+    #' @author Patrick Thiel
     
-    org_data <- haven::read_dta(housing_data_org_file_name)
+    #--------------------------------------------------
+    # read data
+
+    org_data <- arrow::read_parquet(
+        data_file_path
+    )
+
+    #--------------------------------------------------
+    # return
 
     return(org_data)
 }
