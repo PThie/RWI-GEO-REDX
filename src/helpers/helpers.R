0d0e7dad6d674703e0b965e6db39724b78d5ea82
Patrick Thiel <patrickthiel@outlook.de>
2024-05-02 10:02:34 +0200

    Create helpers.R
    Function that specifies helpers (e.g. globals for the estimation). Not stored into config such that the file actually goes to version control and the here specified globals are transparent.

---
+++ src/helpers/helpers.R
@@ -0,0 +1,75 @@
+#--------------------------------------------------
+# global variable definition for estimation
+
+helpers_var_definition <- function(housing_type = NA) {
+    #' @description The function sets the dependent and independent variables
+    #' for the different housing types used in the estimations. Define also
+    #' fixed effects.
+    
+    #--------------------------------------------------
+    # housing characteristics
+
+    var_list <- list()
+    if (housing_type == "HK") {
+        var_list[["depvar"]] <- "ln_houseprice_sqm"
+        var_list[["indepvars"]] <- c(
+            "as.factor(construction_year_cat)",
+            "as.factor(first_occupancy)",
+            "as.factor(gaestewc)",
+            "as.factor(einliegerwohnung)",
+            "as.factor(ausstattung)",
+            "as.factor(zimmeranzahl_full)",
+            "as.factor(plot_area_cat)",
+            "as.factor(typ_freistehend)",
+            "as.factor(typ_DHH)",
+            "as.factor(typ_Reihenhaus)",
+            "as.factor(typ_exclusive)",
+            "as.factor(typ_MFH)",
+            "as.factor(typ_other)"
+        )
+    } else if (housing_type == "WM") {
+        var_list[["depvar"]] <- "ln_rent_sqm"
+        var_list[["indepvars"]] <- c(
+            "as.factor(construction_year_cat)",
+            "as.factor(first_occupancy)",
+            "as.factor(balkon)",
+            "as.factor(garten)",
+            "as.factor(einbaukueche)",
+            "as.factor(gaestewc)",
+            "as.factor(keller)",
+            "as.factor(ausstattung)",
+            "as.factor(zimmeranzahl_full)"
+        )
+    } else {
+        var_list[["depvar"]] <- "ln_flatprice_sqm"
+        var_list[["indepvars"]] <- c(
+            "as.factor(construction_year_cat)",
+            "as.factor(first_occupancy)",
+            "as.factor(balkon)",
+            "as.factor(garten)",
+            "as.factor(einbaukueche)",
+            "as.factor(gaestewc)",
+            "as.factor(aufzug)",
+            "as.factor(keller)",
+            "as.factor(betreut)",
+            "as.factor(ausstattung)",
+            "as.factor(declared_wohngeld)",
+            "as.factor(zimmeranzahl_full)",
+            "as.factor(num_floors_cat)",
+            "as.factor(floors_cat)"
+        )
+    }
+
+    #--------------------------------------------------
+    # fixed effects
+
+    if (housing_type %in% c("HK", "WM", "WK")) {
+        var_list[["regional_fe"]] <- c("ergg_1km")
+        var_list[["time_fe"]] <- c("ejahr", "e_year_quarter", "e_year_mon")
+    }
+
+    #--------------------------------------------------
+    # return
+
+    return(var_list)
+}
