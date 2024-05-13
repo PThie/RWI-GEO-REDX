9601df74d79c68bafb6364e4df675888c69f1459
Patrick Thiel <patrickthiel@outlook.de>
2024-05-13 14:20:30 +0200

    Create aggregating_regional_effects.R
    Function aggregate the estimated effects on grid-time level to a higher geographical unit like district-time or municipality-time.

---
+++ src/others/aggregating_regional_effects.R
@@ -0,0 +1,99 @@
+aggregating_regional_effects <- function(
+    estimated_effects_list = NA,
+    housing_type = NA
+) {
+    #' @title Aggregating regional effects
+    #' 
+    #' @description This function aggregates regional effects for a given housing
+    #' type to a larger regional level (municipality and districts).
+    #' 
+    #' @param estimated_effects_list List with estimated regional effects
+    #' @param housing_type Housing type
+    #' 
+    #' @return List with aggregated regional effects
+    #' @author Patrick Thiel
+    
+    #--------------------------------------------------
+    # loop through time levels on grid level (previously estimated effects)
+
+    result_list <- list()
+    for (result in names(estimated_effects_list)) {
+        for (agg_level in c("munic", "district")) {
+            #--------------------------------------------------
+            # set up for specific regional level
+            # and time definitions
+            
+            if (agg_level == "munic") {
+                nobs_var <- "nobs_munic"
+                region_id <- "gid2019"
+                region_label <- "municipality"
+            } else {
+                nobs_var <- "nobs_district"
+                region_id <- "kid2019"
+                region_label <- "district"
+            }
+
+            if (result == "ejahr") {
+                time_label <- "year"
+            } else {
+                time_label <- "quarter"
+            }
+
+            #--------------------------------------------------
+            # calculate weights
+            # NOTE: weight follows the definition of:
+            # NOBS_ejahr_time_grid / SUM(NOBS_ejahr_time_region)
+            # for each type separately
+            # and region depends on the aggregation level (municipality or district)
+            # and time depends on the time level (year or quarter) (this is given
+            # by the result variable, i.e. the data set itself)
+
+            estimated_effects <- estimated_effects_list[[result]] |>
+                dplyr::mutate(
+                    weight = nobs_grid / .data[[nobs_var]],
+                    weighted_pindex = pindex * weight
+                )
+
+            #--------------------------------------------------
+            # aggregate to higher level   
+
+            estimated_effects_agg <- estimated_effects |>
+                dplyr::group_by(
+                    !!rlang::sym(time_label),
+                    !!rlang::sym(region_id)
+                ) |>
+                dplyr::summarise(
+                    weighted_pindex = sum(weighted_pindex, na.rm = TRUE),
+                    !!nobs_var := dplyr::first(.data[[nobs_var]])
+                )
+
+            #--------------------------------------------------
+            # export
+
+            openxlsx::write.xlsx(
+                estimated_effects_agg,
+                file.path(
+                    config_paths()[["output_path"]],
+                    paste0(housing_type, "_rebuild"),
+                    paste0(
+                        "regional_effects_",
+                        region_label,
+                        "_",
+                        time_label,
+                        ".xlsx"
+                    )
+                )
+            )
+
+            #--------------------------------------------------
+            # store results
+
+            result_list[[paste0(region_label, "_", time_label)]] <- estimated_effects_agg
+        }
+    }
+
+    #--------------------------------------------------
+    # return
+
+    return(result_list)
+}
