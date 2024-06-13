2d50197ec7978b31978fa99f662f3bdfe37ee8ed
Patrick Thiel <patrickthiel@outlook.de>
2024-06-13 08:57:59 +0200

    Generalized the function
    It now can take the regional type and adds housing type to the grid specification.
    
    The regional type allows the application of the function not just to grids but also to the aggregated data.
    
    The housing type needs only be added to the grid version because here are the housing types pooled together and exported into one file/ sheet. For the individual (non-grid) data where are separated sheets for each type.

--- src/helpers/helpers_sorting_columns_region_effects.R
+++ src/helpers/helpers_sorting_columns_region_effects.R
@@ -1,6 +1,8 @@
 helpers_sorting_columns_region_effects <- function(
     region_effects_data = NA,
-    housing_type = NA
+    housing_type = NA,
+    regional_col = NA,
+    grids = TRUE
 ) {
     #' @title Sort columns of region effects data frame
     #' 
@@ -10,18 +12,19 @@ helpers_sorting_columns_region_effects <- function(
     #' 
     #' @param region_effects_data Data frame with region effects
     #' @param housing_type Housing type
+    #' @param regional_col Column names for regional identifier
     #' 
     #' @return Data frame with sorted columns and housing type
     #' @author Patrick Thiel
 
     #--------------------------------------------------
     # extract column names and get rid of the grid column
-    
+
     nam <- names(region_effects_data)
-    nam <- nam[!grepl("grid", nam)]
+    nam <- nam[!grepl(regional_col, nam)]
 
     # extract years and sort them
-    sorted_vec <- c("grid")
+    sorted_vec <- c(regional_col)
     for (year in seq(
         config_globals()[["first_year"]],
         config_globals()[["max_year"]])
@@ -38,13 +41,15 @@ helpers_sorting_columns_region_effects <- function(
 
     #--------------------------------------------------
     # add housing type
+    # only for grids data since this is one combined export
+    
+    if (grids == TRUE) { 
+        region_effects_data$housing_type <- housing_type
 
-    region_effects_data$housing_type <- housing_type
-
-    # move a second position
-    region_effects_data <- region_effects_data |>
-        dplyr::relocate(housing_type, .after = "grid")
-
+        # move a second position
+        region_effects_data <- region_effects_data |>
+            dplyr::relocate(housing_type, .after = regional_col)
+    }
     #--------------------------------------------------
     # return
 
