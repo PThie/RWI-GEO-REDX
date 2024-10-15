297cddaf3d5abe190b9067b908fcec99629faf6d
Patrick Thiel <patrickthiel@outlook.de>
2024-10-15 10:00:55 +0200

    Create helpers_monitoring_pipeline.R
    This function measures the pipeline process (i.e. how many targets are completed,
    skipped etc.) when the pipeline is run. It also measures the performance of the
    workers in parallel processing to get an idea how the work is distributed and if
    potentially more workers are needed.

---
+++ src/helpers/helpers_monitoring_pipeline.R
@@ -0,0 +1,50 @@
+helpers_monitoring_pipeline <- function() {
+    #' @title Monitor pipeline
+    #' 
+    #' @description This function monitors the pipeline and writes the results
+    #' to a log file.
+    #' 
+    #' @return NULL
+    #' @author Patrick Thiel
+    
+    #--------------------------------------------------
+    # pipeline stats
+
+    pipeline_stats <- tar_progress_summary() |>
+        as.data.frame()
+
+    write.table(
+        pipeline_stats,
+        file.path(
+            config_paths()[["logs_path"]],
+            "pipeline_stats.txt"
+        ),
+        sep = "\t\t|",
+        row.names = FALSE
+    )
+
+    #--------------------------------------------------
+    # worker stats
+    
+    worker_stats <- tar_crew() |>
+        as.data.frame()
+        
+    write.table(
+        worker_stats,
+        file.path(
+            config_paths()[["logs_path"]],
+            "worker_stats.txt"
+        ),
+        sep = "\t\t|",
+        row.names = FALSE
+    )
+
+    #--------------------------------------------------
+    # return
+    # NOTE: return can only be a character since the output type has to align
+    # with tar_file (original idea was to return a list with the two dataframes)
+    # NOTE: targets type has to be tar_file in order to use tar_progress_summary
+    # and tar_crew within the pipeline
+
+    return(NULL)
+}
