c4b7e963b4e108d1fd7c5a722c409d0afdd53fbb
Patrick Thiel <patrickthiel@outlook.de>
2024-10-16 12:20:04 +0200

    Create creating_folder_structure.R
    Function that creates the folder structure of a new wave.

---
+++ src/others/creating_folder_structure.R
@@ -0,0 +1,43 @@
+creating_folder_structure <- function() {
+    #' @title Create folder structure
+    #' 
+    #' @description This function creates the folder structure for the output
+    #' of a new wave.
+    #' 
+    #' @return NULL
+    #' @author Patrick Thiel
+
+    #--------------------------------------------------
+    # create type folders with subdirectories
+
+    for (TYPE in config_globals()[["housing_types"]]) {
+        for (FOLDER in config_globals()[["folders"]]) {
+            directory <- file.path(
+                config_paths()[["output_path"]],
+                TYPE,
+                FOLDER
+            )
+
+            if (!dir.exists(directory)) {
+                dir.create(directory, recursive = TRUE)
+            }
+        }
+    }
+
+    #--------------------------------------------------
+    # create export folder
+
+    directory <- file.path(
+        config_paths()[["output_path"]],
+        "export"
+    )
+
+    if (!dir.exists(directory)) {
+        dir.create(directory, recursive = TRUE)
+    }
+
+    #--------------------------------------------------
+    # return
+
+    return(NULL)
+}
