392d861133e28a29872766a4d86a698e994b36e9
Patrick Thiel <patrickthiel@outlook.de>
2024-10-16 15:34:55 +0200

    Fixed error that file exists
    When the pipeline is rerun the processed crashed, because the workbooks already exists. Therefore, implemented a check that skips the workbook creation when they already exist.

--- src/others/creating_export_workbooks.R
+++ src/others/creating_export_workbooks.R
@@ -13,24 +13,30 @@ creating_export_workbooks <- function() {
 
     for (anonym_type in c("PUF", "SUF")) {
         for (data_type in c("ApPurc", "ApRent", "CombInd", "GRIDS", "HouPurc")) {
+            # create empty workbook
             empty_workbook <- openxlsx::createWorkbook()
 
-            openxlsx::saveWorkbook(
-                empty_workbook,
-                file.path(
-                    config_paths()[["output_path"]],
-                    "export",
-                    paste0(
-                        "RWIGEOREDX_",
-                        data_type,
-                        "_",
-                        config_globals()[["next_version"]],
-                        "_",
-                        anonym_type,
-                        ".xlsx"
-                    )
+            # define directory
+            directory <- file.path(
+                config_paths()[["output_path"]],
+                "export",
+                paste0(
+                    "RWIGEOREDX_",
+                    data_type,
+                    "_",
+                    config_globals()[["next_version"]],
+                    "_",
+                    anonym_type,
+                    ".xlsx"
                 )
             )
+
+            if (!file.exists(directory)) {
+                openxlsx::saveWorkbook(
+                    empty_workbook,
+                    file = directory
+                )
+            }
         }
     }
 
