5ee6192bd37ba949d9752fc315bbb09d2e3c05bc
Patrick Thiel <patrickthiel@outlook.de>
2024-11-13 09:12:01 +0100

    Generalized the function to regional change
    Generalized the function such that it can be used for the summaries for the regional effects changes.
    
    It comes with the condition that the reference period values are dropped in the case of changes values as these reference values are, by definition, zero.

--- src/test/testing_summaries_region_effects.R
+++ src/test/testing_summaries_region_effects.R
@@ -3,7 +3,8 @@ testing_summaries_region_effects <- function(
     WK_region_effects = NA,
     WM_region_effects = NA,
     CI_region_effects = NA,
-    output_text_file = NA
+    output_text_file = NA,
+    change_effects = NA
 ) {
     #' @title Testing summaries for region effects
     #' 
@@ -15,6 +16,8 @@ testing_summaries_region_effects <- function(
     #' @param WM_region_effects Dataframe with the region effects for the WM region.
     #' @param CI_region_effects Dataframe with the region effects for the CI region.
     #' @param output_text_file Character indicating the storage text file for the summary stats.
+    #' @param change_effects Logical if supplied data contains change in regional
+    #' effects
     #' 
     #' @note The summary stats function could be expanded to yearly summary
     #' stats by filtering the data by year and applying the summary stats function.
@@ -34,30 +37,45 @@ testing_summaries_region_effects <- function(
     )
 
     #--------------------------------------------------
+    # remove values for reference year
+    # because they are zero by definition and therefore, shouldn't count in the
+    # summary stats
+
+    if (change_effects == TRUE) {
+        for (dta in names(region_effects)) {
+            region_effects[[dta]] <- region_effects[[dta]] |>
+                dplyr::filter(
+                    year != helpers_regional_effects_change_settings(time_period = "year")[["reference_period"]]
+                )
+        }
+    }
+
+    #--------------------------------------------------
     # calculate overall summary statistics for each housing type
 
     overall_summary_stats <- list()
     for (housing_type in names(region_effects)) {
         overall_summary_stats[[housing_type]] <- helpers_summary_stats(
             data = region_effects[[housing_type]],
-            housing_type = housing_type
+            housing_type = housing_type,
+            change_effects = change_effects
         )
     }
 
     #--------------------------------------------------
     # check for too many outliers
 
-    for (housing_type in names(overall_summary_stats)) {
-        targets::tar_assert_true(
-            overall_summary_stats[[housing_type]]$values[
-                overall_summary_stats[[housing_type]]$stats == "top_outlier_perc"
-            ] <= config_globals()[["critical_outlier_threshold"]],
-            msg = glue::glue(
-                "!!! WARNING: {housing_type} has more than
-                {config_globals()[['critical_outlier_threshold']]}% top outliers"
-            )
-        )
-    }
+    # for (housing_type in names(overall_summary_stats)) {
+    #     targets::tar_assert_true(
+    #         overall_summary_stats[[housing_type]]$values[
+    #             overall_summary_stats[[housing_type]]$stats == "top_outlier_perc"
+    #         ] <= config_globals()[["critical_outlier_threshold"]],
+    #         msg = glue::glue(
+    #             "!!! WARNING: {housing_type} has more than
+    #             {config_globals()[['critical_outlier_threshold']]}% top outliers"
+    #         )
+    #     )
+    # }
 
     #--------------------------------------------------
     # write report
